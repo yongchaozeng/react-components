8c1166fff67d7d2ab7cb48ce93d6fdb4c134f9de
zyc <491476277@qq.com>
2020-12-16 19:49:24 +0800

    红宝书注释

--- leam/类型.js
+++ leam/类型.js
@@ -10,6 +10,27 @@ console.log(typeof test); //function
 console.log(typeof { name: 12 }); //object
 console.log(typeof a); //symbol
 // continue
+/**
+ * 类型判断
+ * 
+ * typeof 判断array null都为'object' 不准确
+ * 
+ * Object.prototype.toString.call() 但是必须是原型上的，array函数的tostring被重写过
+ *  优点基本类型判断准确，但是无法判断自定义对象
+ * 
+ * instanceof 检测实例是否属于某个类，不能检测简单类型 通过prototype检测，也可能出现误测
+ *  原理：实例的_proto_和类的prototype对比
+*/
+
+// 作用域 上下文 函数嵌套访问变量
+
+// 垃圾回收 标记法  运行时把内存所有变量标记，然后把在执行上下文的变量标记去除，清楚所有带标记上下文没有访问的变量
+// 内存泄漏
+
+// date regExp
+
+
+//ArrayBuffer  DataView
 
 /** 简单类型：存储在栈中，大小固定，查找更快，复制值
  *  引用类型：存储在堆中，大小不定，查找慢，复制指针
