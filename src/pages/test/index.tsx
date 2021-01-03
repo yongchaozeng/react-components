2aa0160d710ae3723af5c29056b55e368ce6a003
永超 <491476277@qq.com>
2021-01-03 23:27:39 +0800

    feat: 拖拽组件增加mouse事件

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,16 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ReactEventHandler, SyntheticEvent, EventHandler } from 'react';
 import { useTitle, useRequest } from 'ahooks';
+import Input from 'components/Input'
+import Avatar from 'components/Avatar'
+import { YcSelect, YcOption } from 'components/yc-select'
 import './test.less';
 
-
+interface CanvasXY {
+  (e: React.MouseEvent<HTMLElement>): { x: number, y: number }
+}
 
 function Test() {
   let [a, setA] = useState(0)
+
+  const [isMove, setIsMove] = useState(true);
+  const [username, setUsername] = useState('');
+  const [xy, setXy] = useState({
+    x: 0,
+    y: 0
+  });
   let [componentData, setComponentData] = useState<any[]>([{ label: 'test' }])
   let [centerArray, setCenterArray] = useState<any[]>([])
   let arr = [
     {
-      component: 'v-text', // 组件名称，需要提前注册到 Vue
+      component: Input, // 组件名称，需要提前注册到 Vue
       label: '文字', // 左侧组件列表中显示的名字
       propValue: '文字', // 组件所使用的值
       icon: 'el-icon-edit', // 左侧组件列表中显示的名字
@@ -28,7 +40,7 @@ function Test() {
       },
     },
     {
-      component: 'v-text', // 组件名称，需要提前注册到 Vue
+      component: Avatar, // 组件名称，需要提前注册到 Vue
       label: '按钮', // 左侧组件列表中显示的名字
       propValue: '文字', // 组件所使用的值
       icon: 'el-icon-edit', // 左侧组件列表中显示的名字
@@ -44,8 +56,9 @@ function Test() {
         textAlign: '',
         color: '',
       },
-    }, {
-      component: 'v-text', // 组件名称，需要提前注册到 Vue
+    },
+    {
+      component: 'Input', // 组件名称，需要提前注册到 Vue
       label: '输入框', // 左侧组件列表中显示的名字
       propValue: '文字', // 组件所使用的值
       icon: 'el-icon-edit', // 左侧组件列表中显示的名字
@@ -76,41 +89,86 @@ function Test() {
   }, [])
 
 
-  const dragstart = (e: any) => {
-
-    e.dataTransfer.setData('index', e.target.dataset.index)
-
+  const canvasXy: CanvasXY = (e) => {
+    return { x: e.clientX - (e.target as HTMLElement).offsetLeft, y: e.clientY - (e.target as HTMLElement).offsetTop }
   }
-  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
 
+  const dragstart = (e: React.DragEvent<HTMLDivElement>) => {
+    var index = (e.target as HTMLDivElement).dataset.index;
+    if (index) {
+      e.dataTransfer.setData('index', index)
+    }
   }
-  const drop = (e: React.DragEvent<HTMLDivElement>) => {
+
+  const drop = (e: React.DragEvent<HTMLElement>) => {
     let index = e.dataTransfer.getData('index')
-    setCenterArray([...centerArray, componentData[parseInt(index)]])
+    if (index !== '') {
+      setCenterArray(
+        [...centerArray, {
+          ...componentData[parseInt(index)],
+          x: canvasXy(e).x, y: canvasXy(e).y
+        }])
+    }
   }
   const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
+  }
 
+  const MouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsMove(true)
+    console.log(1);
+  }
+  const MouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(2);
+    if (isMove) {
+      setXy(canvasXy(e))
+    }
+  }
+  const MouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // setComponentData(componentData)
+    console.log(3);
+    
+    let index = (e.target as HTMLDivElement).dataset.index
+    if (index) {
+      // console.log(index);
+
+      // componentData[index].x = xy.x
+      // componentData[index].y = xy.y
+    }
+    // console.log();
+
+    setIsMove(false)
   }
   return (
     <div className='container'>
+      {/* 组件列表 */}
       <div onDragStart={(e) => { dragstart(e) }} className='left'>
+        <h6>{`${xy.x}`}</h6>
+        <h6>{`${xy.y}`}</h6>
         {
           componentData.map((item, index) => {
-            return <div className='box' draggable="true" data-index={index} key={index}>{item.label}</div>
+            return <div onMouseUp={MouseUp} className='box' draggable="true" data-index={index} key={index}>{item.label}</div>
           })
         }
       </div>
-      <div onDrop={drop}
-        onDragOver={onDragOver}
-        className='center'>
+      {/*  画布 */}
+      <div onDrop={drop} onDragOver={onDragOver} className='center'>
         {
           centerArray.map((item, index) => {
-            return <div className='box' key={index}>{item.label}</div>
+            let Component = item['component']
+            return (
+              <div data-index={index} className='component-container'
+                onMouseMove={MouseMove}
+                onMouseDown={MouseDown}
+                onMouseUp={MouseUp}
+                style={{ left: item.x, top: item.y }} key={index} >
+                <Component key={index} src={require('../../imgs/login-bg.jpg')} ></Component>
+              </div>
+            )
           })
         }
       </div>
-      <div className='right'></div>
+      {/* <div className='right'></div> */}
     </div>
   );
 
